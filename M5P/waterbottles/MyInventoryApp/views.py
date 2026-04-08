fc0884cdf28992760ff6a8aa0b909e632a6a7cca
gideng <gide.alden.ng@student.ateneo.edu>
2026-04-08 12:07:24 +0800

    made some changes in view.py

--- M5P/waterbottles/MyInventoryApp/views.py
+++ M5P/waterbottles/MyInventoryApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Supplier, WaterBottle, Account
 from django.contrib import messages
 
+current_account = None
 # Create your views here.
 def view_bottles(request):
     waterbottle_objects = WaterBottle.objects.all()
@@ -9,7 +10,10 @@ def view_bottles(request):
 
 def view_supplier(request):
     supplier_objects = Supplier.objects.all()
-    return render(request, 'MyInventoryApp/view_supplier.html', {'suppliers':supplier_objects})
+    return render(request, 'MyInventoryApp/view_supplier.html', {
+        'suppliers':supplier_objects,
+        'account': current_account,
+        })
 
 def view_inventory(request):
     #waterbottle_objects = WaterBottle.objects.all(), {'waterbottles':waterbottle_objects}
@@ -19,12 +23,14 @@ def add_bottle(request):
     return render(request, 'MyInventoryApp/add_bottle.html')
 
 def login_view(request):
+    global current_account
     if request.method == 'POST':
         u = request.POST.get('username')
         p = request.POST.get('password')
         
         account = Account.objects.filter(username=u, password=p).first()
         if account: 
+            current_account = account
             return redirect('view_supplier')
         else: 
             messages.error(request, 'Invalid login')
@@ -44,3 +50,8 @@ def signup_view(request):
             return redirect('login')
             
     return render(request, 'MyInventoryApp/signup.html')
+
+def logout_view(request):
+    global current_account
+    current_account = None
+    return redirect('login')
